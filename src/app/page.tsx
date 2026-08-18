023af88448f67a7c71c0e5494c1c0c64c6778b74
Honey sharma <114321260+honey9876@users.noreply.github.com>
2026-08-18 15:03:48 +0530

    fix: resolve duplicate homepage route causing prerender error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,27 @@
 import { useAuth } from "@/features/auth/hooks/useAuth";
 import { useEducation } from "@/features/profile/hooks/useEducation";
 import { useProfile } from "@/features/profile/hooks/useProfile";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import HeroSection from "@/features/public/components/HeroSection";
 
 export default function Home() {
+  const router = useRouter();
+  const { user, isLoading } = useAuth();
+  const { loadProfile, loadPosts } = useProfile();
+  const { loadEducation } = useEducation();
 
- const router= useRouter();
-   const { user, isLoading } = useAuth();
- 
-   const { loadProfile, loadPosts } = useProfile();
-   const { loadEducation } = useEducation();
- 
-   useEffect(() => {
-     if (user) {
-       loadProfile();   // ← Redux action
-       loadPosts();
-       loadEducation();
-     }
-   }, [user]);
+  useEffect(() => {
+    if (user) {
+      loadProfile();
+      loadPosts();
+      loadEducation();
+      router.replace("/dashboard");
+    }
+  }, [user]);
 
-   redirect(user ? "/dashboard" : "/login");
+  if (isLoading) return null;
+  if (user) return null;
+
+  return <HeroSection />;
 }
